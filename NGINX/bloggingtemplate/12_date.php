8e91ca42e62e29d43f8afce9dbe267bab19c8aa5
Sergey <43184096+b-c-v@users.noreply.github.com>
2024-12-21 01:05:28 +0100

    NGINX: fastcgi_cache

---
+++ NGINX/bloggingtemplate/12_date.php
@@ -0,0 +1,3 @@
+<?php sleep(10); ?>
+<h1> Today's Date : <?php echo date("l js F"); // output the current day of the week, the day of the month with a suffix, and the full month name?> </h1>
+<h2><?php echo date('H:i:s'); // Outputs the current time in the format YYYY-MM-DD HH:MM:SS ?></h2>
