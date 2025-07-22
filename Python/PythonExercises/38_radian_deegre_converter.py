8cba601505c043eaa734badae740abcf55afb95b
Sergey <43184096+b-c-v@users.noreply.github.com>
2025-07-22 23:44:22 +0200

    Python: add exercises 38-40

---
+++ Python/PythonExercises/38_radian_deegre_converter.py
@@ -0,0 +1,5 @@
+import math
+
+number = int(input("Enter any number: "))
+print(f"{number} radians converted to degrees {math.degrees(number)}")
+print(f"{number} degrees converted to radians {math.radians(number)}")
---
+++ Python/PythonExercises/40_function_check_username.py
@@ -0,0 +1,13 @@
+username = input("Enter your username: ")
+
+
+def check_username(any_username):
+    if any_username.isalnum() and len(any_username) >= 8:
+        print("Your any_username is correct")
+    else:
+        print(
+            "Your username must contain only letters and digits and must be longer than 7 characters."
+        )
+
+
+check_username(username)
