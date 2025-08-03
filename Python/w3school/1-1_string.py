4c739189f9df68899714e4752e95a8d74107a48d
Sergey <43184096+b-c-v@users.noreply.github.com>
2025-08-03 22:48:13 +0200

    Python: explain isidentifier() method for string

--- Python/w3school/1-1_string.py
+++ Python/w3school/1-1_string.py
@@ -120,7 +120,7 @@ isalnum()          Returns True if all characters in the string are alphanumeric
 isalpha()          Returns True if all characters in the string are in the alphabet
 isdecimal()        Returns True if all characters in the string are decimals
 isdigit()          Returns True if all characters in the string are digits
-isidentifier()     Returns True if the string is an identifier
+isidentifier()     Returns True if the string is an identifier - то есть именем переменной, функции, класса и т. д., по правилам языка Python
 islower()          Returns True if all characters in the string are lower case
 isnumeric()        Returns True if all characters in the string are numeric
 isprintable()      Returns True if all characters in the string are printable
