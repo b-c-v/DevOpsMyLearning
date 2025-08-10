d169e7b6d5ff3ba7d87cfd529086473078de1ea7
Sergey <43184096+b-c-v@users.noreply.github.com>
2025-08-10 21:58:28 +0200

    Python: enumerate function

--- Python/w3school/7-1_built-in-functions.py
+++ Python/w3school/7-1_built-in-functions.py
@@ -16,7 +16,7 @@ delattr()          Deletes the specified attribute (property or method) from the
 dict()             Returns a dictionary (Array)
 dir()              Returns a list of the specified object's properties and methods
 divmod()           Returns the quotient and the remainder when argument1 is divided by argument2
-enumerate()        Takes a collection (e.g. a tuple) and returns it as an enumerate object
+enumerate()        Takes a collection (e.g. a tuple) and returns it as an enumerate object. Добавляет к каждому элементу последовательности его индекс (порядковый номер), начиная с 0
 eval()             Evaluates and executes an expression
 exec()             Executes the specified code (or object)
 filter()           Use a filter function to exclude items in an iterable object
@@ -103,10 +103,10 @@ print(x)  # ['h', 'g', 'f', 'e', 'd', 'c', 'b', 'a']
 # Функция ord() позволяет определить код некоторого символа в таблице символов Unicode. Аргументом данной функции является одиночный символ.
 # Если попытаться передать строку, содержащую более одного символа, получим ошибку времени выполнения: TypeError: ord() expected a character, but string of length 3 found
 # Название функции ord происходит от английского слова order — порядок.
-print('===ord()===')
-num1 = ord('A')
-num2 = ord('B')
-num3 = ord('a')
+print("===ord()===")
+num1 = ord("A")
+num2 = ord("B")
+num3 = ord("a")
 print(num1, num2, num3)  # 65 66 97
 
 
@@ -121,7 +121,19 @@ print(chr1, chr2, chr3)  # A B a
 # Вывод всех заглавных букв английского алфавита.
 # Вызов функции ord('A') возвращает код символа «A», который равен 65. Далее на каждой итерации цикла, к данному коду прибавляется значение переменной i = 0, 1, 2, ..., 25, а затем полученный код преобразуется в символ с помощью вызова функции chr.
 for i in range(26):
-    print(chr(ord('A') + i), end='')  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
+    print(chr(ord("A") + i), end="")  # ABCDEFGHIJKLMNOPQRSTUVWXYZ
 
 # Функции ord и chr являются взаимнообратными. Для них выполнены равенства:
 # chr(ord('A')) = 'A', ord(chr(65)) = 65.
+
+
+# enumerate() возвращает кортежи вида (индекс, элемент).
+# x - это индекс (начинается с 0)
+# y - это значение из range(2) (т.е. 0, 1)
+for x, y in enumerate(range(2)):
+    print(f"x - {x}; y - {y}")
+    print(f"{chr(ord('A') + x)}")
+
+# Что будет, если оставить только x, то x будет не индексом, а кортежем (индекс, значение).
+for i in enumerate(range(2)):
+    print(f"enumerate(): {i}")  # (0, 0) (1, 1)
