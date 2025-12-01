b9677456d1b8ea8c7a92628022e0e650d6e76f42
Sergii Boboshko <sergii.boboshko@henesis.eu>
2025-12-01 09:38:07 +0100

    Python: try-except

--- Python/w3school/15_try-except.py
+++ Python/w3school/15_try-except.py
@@ -16,6 +16,19 @@ except:
 # Without the try block, the program will crash and raise an error:
 # print(x) # NameError: name 'x' is not defined
 
+# except can handle specific exception types
+try:
+    number = int("any_text")
+except ValueError:
+    print("A ValueError occurred")
+
+# To handle errors correctly, you must first understand which exceptions a statement can raise.
+try:
+    number = int("any_text")
+except Exception as error:
+    print(f"Exception type: {type(error).__name__}")  # ValueError
+    print(f"Error message: {error}")
+
 # Many Exceptions. You can define as many exception blocks as you want, e.g. if you want to execute a special block of code for a special kind of error:
 # Print one message if the try block raises a NameError and another for other errors:
 try:
@@ -24,6 +37,7 @@ except NameError:
     print("Variable x is not defined")
 except:
     print("Something else went wrong")  # Variable x is not defined
+
 
 # Else. You can use the else keyword to define a block of code to be executed if no errors were raised:
 # In this example, the try block does not generate any error:
@@ -57,7 +71,8 @@ except:
     # Something went wrong when opening the file
     print("Something went wrong when opening the file")
 
-# Raise an exception. As a Python developer you can choose to throw an exception if a condition occurs.
+# Raise an exception.
+# As a Python developer you can choose to throw an exception if a condition occurs.
 # To throw (or raise) an exception, use the raise keyword.
 # Raise an error and stop the program if x is lower than 0:
 x = -1
@@ -65,9 +80,25 @@ if x < 0:
     # Exception: Sorry, no numbers below zero
     raise Exception("Sorry, no numbers below zero")
 
-# The raise keyword is used to raise an exception.
 # You can define what kind of error to raise, and the text to print to the user.
 # Raise a TypeError if x is not an integer:
 x = "hello"
 if not type(x) is int:
     raise TypeError("Only integers are allowed")
+
+
+# Raising and handling custom exceptions.
+# Custom exceptions must inherit from Exception and except block must reference the exact exception class
+
+
+class MyCustomError(Exception):
+    print("Raised when a value is not of the expected type.")
+    pass
+
+
+try:
+    value = "hello"
+    if not isinstance(value, int):
+        raise MyCustomError("Only integers are allowed")  # raising a Custom Exception
+except MyCustomError:  # catching the Custom Exception
+    print("A custom type error occurred")
