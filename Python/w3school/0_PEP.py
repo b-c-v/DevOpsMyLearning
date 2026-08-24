b9a43d24ea8c076c5dea612919c861b553c1b0bf
Sergii Boboshko <sergii.boboshko@rockwellautomation.com>
2026-08-24 12:41:46 +0200

    Python: add PEP documentation

---
+++ Python/w3school/0_PEP.py
@@ -0,0 +1,27 @@
+# PEP = Python Enhancement Proposal - документ с предложением по языку Python
+# (новая фича, процесс разработки, руководство/guideline). Похоже на RFC в других
+# технических стандартах. 
+# https://peps.python.org/
+
+# У каждого PEP есть номер (по порядку подачи предложения) и статус:
+# Draft -> Accepted / Rejected -> Final (или Superseded, Withdrawn)
+
+
+# ***Три типа PEP***
+"""
+Standards Track  - предлагает новую фичу или изменение в языке/стандартной библиотеке (большинство PEP)
+Informational    - описывает рекомендации/информацию для сообщества, не меняет язык (например, PEP 8)
+Process          - описывает процесс, связанный с самим Python (например, как проходят релизы)
+"""
+
+
+# ***PEP, которые пригодятся при изучении Python***
+"""
+PEP 8     - Style Guide for Python Code (именование, отступы, пробелы) - https://peps.python.org/pep-0008/
+PEP 484   - Type Hints (синтаксис и смысл аннотаций типов, см. 7_functions.py) - https://peps.python.org/pep-0484/
+PEP 20    - The Zen of Python (философия языка, доступна командой import this) - https://peps.python.org/pep-0020/
+PEP 257   - Docstring Conventions - https://peps.python.org/pep-0257/
+PEP 3107  - Function Annotations (исходный синтаксис аннотаций, позже PEP 484 придал ему смысл типов) - https://peps.python.org/pep-3107/
+"""
+
+import this  # выводит "The Zen of Python" (PEP 20)
